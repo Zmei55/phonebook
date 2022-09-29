6157b1c53dd58ae0309609432144657605c919b9
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-09-29 15:43:07 +0200

    update

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
 export const ContainerStyle = styled.div`
-  max-width: 1440px;
+  max-width: 1200px;
+  margin-right: auto;
+  margin-left: auto;
   padding: 8px 15px;
 `;
