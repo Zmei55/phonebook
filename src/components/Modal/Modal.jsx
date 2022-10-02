579bda6cb39924e2881556f3beaf15f2a1d51a36
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-10-02 17:57:44 +0200

    modal handleKeyDown

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,26 @@
-// import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Backdrop, Content } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export function Modal({ children, onClose }) {
-  // useEffect(() => {
-  //   window.addEventListener('keydown', handleKeyDown);
-  // }, [handleKeyDown]);
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  // useEffect(() => {
-  //   window.removeEventListener('keydown', handleKeyDown);
-  // }, [handleKeyDown]);
-
-  // const handleKeyDown = event => {
-  //   if (event.code === 'Escape') {
-  //     onClose();
-  //   }
-  // };
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
   const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
